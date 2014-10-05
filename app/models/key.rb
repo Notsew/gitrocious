3a130969b17e90b231c5a2533071187a810ee9cf
Chris Weston <notsew66@yahoo.com>
2014-10-05 12:37:05 -0400

    removing keys on deletion and adding background

--- app/models/key.rb
+++ app/models/key.rb
@@ -3,9 +3,18 @@ class Key < ActiveRecord::Base
 
   validates :value, :user, presence: true
 
+  after_create :add_to_file
+  after_destroy :remove_from_file
+  
   private
 
-  def remove_from_file
+  def add_to_file
+  	open("#{ENV['HOME']}/.ssh/authorized_keys", 'a') do |f|
+	  f.puts self.value
+	end
+  end
 
+  def remove_from_file
+  	`sed -i.bak "/#{self.value}/d" "#{ENV['HOME']}/.ssh/authorized_keys"`
   end
 end
