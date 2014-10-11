7faca6167bc9222eeab19acc1955b5b3ebae3918
Chris Weston <notsew66@yahoo.com>
2014-10-11 16:22:48 -0400

    changing schema

--- db/migrate/20141004172528_create_join_table_group_repo.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableGroupRepo < ActiveRecord::Migration
-  def change
-    create_join_table :groups, :repos do |t|
-       t.index [:group_id, :repo_id]
-       t.index [:repo_id, :group_id]
-    end
-  end
-end
