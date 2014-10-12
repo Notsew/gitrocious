32e99753608e25d2cc670e3683b0ee7933ab846f
Chris Weston <notsew66@yahoo.com>
2014-10-11 23:39:23 -0400

    fixing variables

--- set_variables.rb
+++ set_variables.rb
@@ -1,8 +1,14 @@
 #!/usr/bin/env ruby
+require APP_PATH = File.expand_path('../config/application',  __FILE__)
+require File.expand_path('../config/boot',  __FILE__)
+# set Rails.env here if desired
+Rails.env="production"
+Rails.application.require_environment!
+
 command = ENV["SSH_ORIGINAL_COMMAND"]
 ENV["GITROCIOUS_REPO_NAME"] = command.split(" ")[1].tr("'","")
 ENV["GITROCIOUS_USER_KEY"] = ARGV[0]
-ENV["GITROCIOUS_FILE_LOCATION"] = `pwd`
+ENV["GITROCIOUS_FILE_LOCATION"] = Rails.root.to_s
 Dir.chdir(Rails.application.config.repo_location) do |f|
 	system(command)
 end
